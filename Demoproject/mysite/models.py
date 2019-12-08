b7545cfeb30b4821d6d176ef3e1ba84a4ed4b465
udaybalugi <57953353+udaybalugi@users.noreply.github.com>
2019-12-08 09:21:10 +0530

    Update models.py

--- Demoproject/mysite/models.py
+++ Demoproject/mysite/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ClassName(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+		
 
 
